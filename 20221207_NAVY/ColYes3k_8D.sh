cfd9cbfe24b5726629d38e05ab2b55f878f3facf
hannania7 <69201715+hannania7@users.noreply.github.com>
2022-12-09 15:14:22 +0900

    Delete ColYes3k_8D.sh

--- 20221207_NAVY/ColYes3k_8D.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export NCARG_ROOT=/usr/local/ncl-6.6.2
-export PATH=$NCARG_ROOT/bin:$PATH
-
-today=`date +%Y%m%d`
-
-/usr/bin/python3.6 /DATA/NAVY/source/ColInit2.py 244 $today 9 >> /DATA/NAVY/source/ColMain.log
